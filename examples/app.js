31e7088520e50bea7b0d52db860e2a8d9d508935
Ganapathy <ganapathy888@gmail.com>
2017-11-15 23:45:19 +0530

    Add React JSX Support

---
+++ examples/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function App(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+// Export
+export default App;
--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,9 @@
-console.log("HI from examples")
+// Vendor Imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Local Imports
+import App from './app';
+
+// App Rendering
+ReactDOM.render(<App />,  document.getElementById('root'));
