6069467a5ba3cba8dad6da88fef44da6a202f30e
Ganapathy <ganapathy888@gmail.com>
2017-11-17 01:35:37 +0530

    Use npx to build source files into Lib

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,32 +5,32 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // Export
 module.exports = merge(common, {
- entry: {
-  app: './examples/index.js'
- },
- devtool: 'inline-source-map',
- devServer: {
-   contentBase: './dist'
- },
- module: {
-     rules: [{
-         test: /\.scss$/,
-         use: [{
-             loader: "style-loader" // creates style nodes from JS strings
-         }, {
-             loader: "css-loader" // translates CSS into CommonJS
-         }, {
-             loader: "sass-loader" // compiles Sass to CSS
-         }]
-     }]
- },
- plugins: [
-   new CopyWebpackPlugin(
-   [
-     {
-       from: './examples/index.html',
-       to: 'index.html'
-     }
-   ])
- ]
+  entry: {
+    app: './examples/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+         loader: "style-loader" // creates style nodes from JS strings
+      }, {
+         loader: "css-loader" // translates CSS into CommonJS
+      }, {
+         loader: "sass-loader" // compiles Sass to CSS
+      }]
+    }]
+  },
+   plugins: [
+     new CopyWebpackPlugin(
+     [
+       {
+         from: './examples/index.html',
+         to: 'index.html'
+       }
+     ])
+   ]
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,38 @@
+// Imports
+const path = require('path');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+// SASS
+const extractSass = new ExtractTextPlugin({
+  filename: "style.css"
+});
+
+// Export
 module.exports = merge(common, {
+  entry: {
+    app: './src/index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: extractSass.extract({
+        use: [{
+            loader: "css-loader"
+        }, {
+            loader: "sass-loader"
+        }]
+      })
+   }]
+ },
  plugins: [
-   new UglifyJSPlugin()
- ]
+   new UglifyJSPlugin(),
+   extractSass
+ ],
+ output: {
+   filename: '[name].min.js',
+   path: path.resolve(__dirname, 'dist')
+ }
 });
