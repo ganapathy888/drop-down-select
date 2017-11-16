db2ecf602f8943fd64c74735e3d8b1b0dfc34c44
Ganapathy <ganapathy888@gmail.com>
2017-11-16 14:00:19 +0530

    Create Async Version of Simple Dropdown Select

--- examples/app.js
+++ examples/app.js
@@ -3,13 +3,21 @@ import React from 'react';
 
 // Local Imports
 import DropdownSelect from '../src/DropdownSelect';
+import { AsyncDropdownSelect } from '../src/DropdownSelect';
 
 function App(props) {
   const optionsArray = ['first', 'second', 'third', '4', '5'];
   return (
     <div style={ {width: '300px'} }>
-      <DropdownSelect options={optionsArray} />
+      <div>
+        <DropdownSelect options={optionsArray} />
+      </div>
+
+      <div>
+        <AsyncDropdownSelect />
+      </div>
     </div>
+
   );
 }
 
