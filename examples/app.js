69c4d2255b5169f1b3a62e36b4fbf10d4493b048
Thanga Ganapathy <ganapathy888@gmail.com>
2017-12-29 22:34:27 +0530

    Fix arrow position

--- examples/app.js
+++ examples/app.js
@@ -2,47 +2,21 @@
 import React, { Component } from 'react';
 
 // Local Imports
+import Layout from './layout';
 import SimpleSelectForm from './components/SimpleSelectForm';
 import AsyncSelectForm from './components/AsyncSelectForm';
 import MultiSelectFormExample from './components/MultiSelectFormExample';
+
+import { Select } from '../src';
 
 class App extends Component {
   // Render
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-dark bg-dark justify-content-between">
-          <a className="navbar-brand" href="#">
-            DROPDOWN SELECT <span className="text-success">(3.0.0)</span>
-            <span className="text-warning"> (DEMO)</span>
-          </a>
-          <ul className="nav">
-            <li className="nav-item">
-              <a
-                className="nav-link active"
-                href="https://github.com/ganapathy888/dropdown-select/"
-              >
-                Github
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://www.npmjs.com/package/dropdown-select"
-              >
-                Npm
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://yarnpkg.com/en/package/dropdown-select"
-              >
-                Yarn
-              </a>
-            </li>
-          </ul>
-        </nav>
+      <Layout>
+        <div style={{ padding: '25px' }}>
+          <Select options={['test']} value="" />
+        </div>
         <div className="container">
           <div className="row mt-5">
             <div className="col-lg-4">
@@ -70,7 +44,7 @@ class App extends Component {
             </div>
           </div>
         </div>
-      </div>
+      </Layout>
     );
   }
 }
---
+++ examples/layout/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import Header from './header';
+
+const Layout = props => {
+  return (
+    <Fragment>
+      <Header />
+      {props.children}
+    </Fragment>
+  );
+};
+
+export default Layout;
