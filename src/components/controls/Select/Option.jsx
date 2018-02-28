f5080e47bf5044f7cfb8003229b119c0d8873646
Thanga Ganapathy <ganapathy888@gmail.com>
2018-02-28 14:17:40 +0530

    wip

--- src/components/controls/Select/Option.jsx
+++ src/components/controls/Select/Option.jsx
@@ -7,26 +7,33 @@ import classNames from '../../../utils/classNames';
 // Option Component
 class Option extends Component {
   // Private
-  _classes() {
+  classes() {
     const { isSelected, isFocused, optionClassName } = this.props;
     return classNames(
       {
         'dropdown-select__options__option': !optionClassName,
         'dropdown-select__options__option--focused': isFocused,
-        'dropdown-select__options__option--selected': isSelected
+        'dropdown-select__options__option--selected': isSelected,
       },
-      optionClassName
+      optionClassName,
     );
   }
 
   // Render
   render() {
-    const { option, index, labelKey } = this.props;
+    const {
+      option, index, labelKey, isSelected,
+    } = this.props;
     return (
       <div
-        className={this._classes()}
+        tabIndex="-1"
+        role="option"
+        aria-selected={isSelected}
+        className={this.classes()}
         onClick={() => this.props.onClick(option)}
         onMouseOver={() => this.props.onMouseOver(index)}
+        onFocus={() => undefined}
+        onKeyDown={() => undefined}
       >
         {labelKey ? option[labelKey] : option}
       </div>
